9bd161ae83dbb21520e1ed4eb19bbd030bedfaa5
Cam @ Camtoso <cameron_dwyer@hotmail.com>
2020-06-17 23:19:29 +1000

    Scaffolded and MGT MsalProvider imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Providers, MsalProvider } from '@microsoft/mgt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo-mgt-angular';
+
+  ngOnInit()
+  {
+    Providers.globalProvider = new MsalProvider({ clientId: '889da468-f719-49b0-8e32-265d5f86ced2' });
+  }
 }
