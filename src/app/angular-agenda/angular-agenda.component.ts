e80a05b9faac7358cc703b0b9dd7ad079636e232
Cam @ Camtoso <cameron_dwyer@hotmail.com>
2020-06-18 11:34:57 +1000

    login working

---
+++ src/app/angular-agenda/angular-agenda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-agenda',
+  templateUrl: './angular-agenda.component.html',
+  styleUrls: ['./angular-agenda.component.css']
+})
+export class AngularAgendaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { basename } from 'path';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
